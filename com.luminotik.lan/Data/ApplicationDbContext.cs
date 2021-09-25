d38e4e8888e919ba3160845217e1b8ce32a64249
nickn <nickn@NICK-PC>
2021-09-25 12:22:47 -0400

    Add project files.

---
+++ com.luminotik.lan/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.luminotik.lan.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
